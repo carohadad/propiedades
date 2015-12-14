886b65110f9bf4174c3512669efd4f62589faf47
Carolina Hadad <carolinahadad@gmail.com>
2015-12-14 14:13:19 -0300

    Cambios menores en el test

--- propiedades/webapp/test_models.py
+++ propiedades/webapp/test_models.py
@@ -8,29 +8,29 @@ from __future__ import unicode_literals
 from __future__ import print_function
 from __future__ import with_statement
 
+import nose
 from django.test import TestCase
-
-from webapp.models import Preferences
 from django.contrib.auth.models import User
 
-
-import unittest
-import nose
+from webapp.models import Preferences
 
 
 class PreferencesTestCase(TestCase):
     """Tests for Preferences class."""
 
     def test_create_new_preference(self):
+        """ Tests default Preference creation. """
+        initial_count = len(Preferences.objects.all())
+
         user = User(email='caro@mail.com')
         user.save()
 
         new_prefernce = Preferences(
-            user_id=user.id, max_price=123, min_price=12)
+            user=user, max_price=123, min_price=12)
 
         new_prefernce.save()
 
-        self.assertEqual(len(Preferences.objects.all()), 1)
+        self.assertEqual(len(Preferences.objects.all()), initial_count + 1)
 
 
 if __name__ == '__main__':
